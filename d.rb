b2b1653d5187fa5231a060ded85f714e623b05ee
dezhaoli <dezhaoli@tencent.com>
2022-06-02 16:35:22 +0800

    add d tools

---
+++ d.rb
@@ -0,0 +1,8 @@
+# This is an auto-generated file. DO NOT EDIT
+class D < Formula
+    desc "Parser for command-line options, arguments and sub-commands"
+    homepage "https://github.com/dezhaoli"
+    version "1.0.0"
+
+    depends_on "xargparse"
+end
