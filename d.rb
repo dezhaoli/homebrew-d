22a00fb048d8cd1d735f5b39edda65f2d39ccbd1
dezhaoli <582339782@qq.com>
2023-06-26 11:35:24 +0800

    v3.1.6

--- d.rb
+++ d.rb
@@ -5,12 +5,12 @@ require_relative "lib/private_strategy"
 class D < Formula
     desc "X toolkit"
     homepage "https://github.com/dezhaoli/d"
-    version "v3.1.5"
+    version "v3.1.6"
 
-    url "https://github.com/dezhaoli/d/archive/v3.1.5.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+    url "https://github.com/dezhaoli/d/archive/v3.1.6.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
 
     if OS.mac?
-      sha256 "9c4defe762b8b1a5a2c8f20ca2dd33d72667717f4ab69a686b92ad021e8369da"
+      sha256 "80612b90bd44043b13f5c1227acb2d00c8935c173a8098971ac132e1d0c3d70b"
     end
 
 
